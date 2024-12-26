176b3d7f67dfaacd89986cf0773ff839f4a670f7
cjmess01 <cjmess01@gmail.com>
2024-12-25 21:30:52 -0600

    added the surround cmds for ([{ and quotes

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -8,11 +8,11 @@ vim.keymap.set("i", "jj", "<esc>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<C-d>","<C-d>zz")
-vim.keymap.set("n", "<C-u>","<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 
-vim.keymap.set("n", "<C-u>","<C-u>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 
 -- greatest remap ever
@@ -24,6 +24,12 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("v", "<Tab>", ">gv")
 vim.keymap.set("v", "<S-Tab>", "<gv")
 
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- vim.keymap.set("v", "<leader>g0", "di)<esc>Pi(<esc>v", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>(", "di(<esc>pa)<esc>v%", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>[", "di[<esc>pa]<esc>v%", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>{", "di{<esc>pa}<esc>v%", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>\"", "di\"<esc>pa\"<esc>vF\"", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>\'", "di\'<esc>pa\'<esc>vF\'", { noremap = true, silent = true })
 
