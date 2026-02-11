b9b6f812d527509c34abbe94fa78ca4f100a9662
cjmess01 <cjmess01@gmail.com>
2026-02-10 22:02:35 -0600

    Added autopairs

--- tests/levi.py
+++ tests/levi.py
@@ -9,9 +9,13 @@ def my_fun(name : str):
     print("hey")
     print("Hey now")
 
+def my_new_fun(name: str):
+    print(name)
+
 my_fun("levi")
 my_fun("caleb")
 my_fun("nathan")
 my_fun("nick")
+my_new_fun("HeU")
 
 
